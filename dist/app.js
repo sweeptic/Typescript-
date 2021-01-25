ec394f3682621da48d2e7f0ebbf5806c4fbba135
unknown <sweeptic@gmail.com>
2021-01-25 17:31:09 +0100

    4. Next-generation JavaScript & TypeScript

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,48 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 console.log('hello again...');
-var button = document.getElementById('button1');
+var add = function (a, b) { return a + b; };
+var printOutput = function (output) { return console.log(output); };
+var button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', function (event) { return console.log(event); });
+}
+var add2 = function (a, b) {
+    if (b === void 0) { b = 1; }
+    return a + b;
+};
+var hobbies = ['sport', 'cooking'];
+var hobbies2 = __spreadArrays(['hiking'], hobbies);
+var person = {
+    firstName: 'Attila',
+    age: 11,
+};
+var copiedPerson = __assign({}, person);
+var add3 = function () {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+};
+var addedNumbers = add3(4, 4, 2, 5, 6, 3.5);
+var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
+console.log(hobby1, hobby2, hobbies);
+var userName = person.firstName, age = person.age;
+console.log(userName, age, person);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,69 @@
 console.log('hello again...');
 
-const button = document.getElementById('button1');
+/* LET CONST */
+// const userName = 'A';
+// userName = 'Attila';
+
+// var age = 33;
+
+// const add = (a: number, b: number) => a + b;
+
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+/*ARROW FUNCTION*/
+// const add = (a: number, b: number) => a + b;
+const add: (a: number, b: number) => number = (a, b) => a + b;
+
+// const printOutput = (output: number | string) => console.log(output);
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+/*DEFAULT FUNCTION PARAMETER*/
+
+const add2 = (a: number, b: number = 1) => a + b;
+// const add2: (a: number, b: number ) => number = (a, b = 1) => a + b; ??
+// console.log(add2(3));
+
+/* THE SPREAD OPERATOR*/
+const hobbies = ['sport', 'cooking'];
+const hobbies2 = ['hiking', ...hobbies];
+
+// hobbies.push(...hobbies2);
+
+// console.log(hobbies2);
+
+const person = {
+  firstName: 'Attila',
+  age: 11,
+};
+
+const copiedPerson = { ...person };
+
+// console.log(copiedPerson);
+
+/*REST PARAMETERS*/
+
+const add3 = (...args: number[]) => {
+  //   console.log(...args);
+};
+
+const addedNumbers = add3(4, 4, 2, 5, 6, 3.5);
+
+// console.log(addedNumbers);
+
+/*ARRAY OBJECT DESTRUCTURING*/
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobby1, hobby2, hobbies);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
